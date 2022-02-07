f6a75d6f9455083d1628d8ec40490a65b83db0b6
StefansArya <stefansarya1@gmail.com>
2022-02-07 15:25:52 +0700

    Fix Blocks that was not updating when have a new input value

--- src/Events/Blocks.js
+++ src/Events/Blocks.js
@@ -31,6 +31,51 @@ class BlocksNode extends Blackprint.Node {
 		let iface = this.setInterface('BPIC/Polkadot.js/Events/Blocks');
 		iface.title = "Blocks Event";
 		iface.description = "Listen for new blocks";
+
+		this._toast = new NodeToast(iface);
+	}
+
+	// This will be called by the engine after the node has been loaded
+	// and other data like cable connection has been connected/added
+	init(){
+		let { IInput } = this.ref; // Shortcut
+
+		// Listen if the cable was disconnected from the input port
+		IInput.API.on('disconnect', Context.EventSlot, ()=> {
+			this.iface.unsubscribe();
+		});
+	}
+
+	// This will be called by the engine when this node is deleted
+	destroy(){
+		this.iface.unsubscribe();
+	}
+
+	// This will be called by the engine if the input port have a new value
+	async update(){
+		let { Input, Output } = this.ref; // Shortcut
+
+		// If this node is already subscribing to blocks event, let's unsubscribe it
+		this.iface.unsubscribe();
+
+		let api = Input.API;
+		if(!api) return;
+
+		if(!api.hasSubscriptions)
+			return this._toast.error("Please use WebSocket for using this feature");
+
+		this._toast.clear();
+
+		// Subscribe to new heads/blocks and save the 'unsubscribe' function
+		this._toast.warn("Subscribing...");
+		this.iface.unsubscribe = await api.rpc.chain.subscribeNewHeads(header => {
+			Output.Data = header;
+			Output.Number = this.iface.number = header.number.toNumber();
+		});
+
+		// Clear the toast
+		this._toast.clear();
+		this._toast.success("Subscribed!");
 	}
 });
 
@@ -42,46 +87,8 @@ Context.IFace.EventsBlocks = class BlocksIFace extends Blackprint.Interface {
 		super(node);
 
 		this.number = 0; // blocks number
-		this.unsubscribe = false; // callable to unsubcribe event
 
-		this._toast = new NodeToast(this);
-	}
-
-	// This will be called by the engine after the node has been loaded
-	// and other data like cable connection has been connected/added
-	init(){
-		let { IInput } = this.ref; // Shortcut
-
-		// Listen if the cable was disconnected from the input port
-		IInput.API.on('disconnect', Context.EventSlot, ()=> {
-			if(this.unsubscribe) this.unsubscribe();
-		});
-	}
-
-	// This will be called by the engine if the input port have a new value
-	async update(){
-		let { Input, Output } = this.ref; // Shortcut
-
-		// If this node is already subscribing to blocks event, let's unsubscribe it
-		if(this.unsubscribe) this.unsubscribe();
-
-		let api = Input.API;
-		if(!api) return;
-
-		// Subscribe to new heads/blocks and save the 'unsubscribe' function
-		this._toast.warn("Subscribing...");
-		this.unsubscribe = await api.rpc.chain.subscribeNewHeads(header => {
-			Output.Data = header;
-			Output.Number = this.number = header.number.toNumber();
-		});
-
-		// Clear the toast
-		this._toast.clear();
-		this._toast.success("Subscribed!");
-	}
-
-	// This will be called by the engine when this node is deleted
-	destroy(){
-		if(this.unsubscribe) this.unsubscribe();
+		// This will be replaced if subcribing to an event, default: no operation
+		this.unsubscribe = ()=> {};
 	}
 });
