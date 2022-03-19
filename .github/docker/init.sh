72794285b8d88e1720d11d98b9abb11368ff7940
StefansArya <stefansarya1@gmail.com>
2022-03-20 01:09:54 +0700

    Add dockerfile

---
+++ .github/docker/init.sh
@@ -0,0 +1,7 @@
+# Create app folder for our project directory and clone the repositories
+mkdir ./app
+git clone --depth 1 --recurse-submodules https://github.com/Blackprint/Blackprint.git ./app
+git clone --depth 1 https://github.com/Blackprint/nodes-polkadot.js.git ./app/nodes/nodes-polkadot.js
+
+# Run the container (force recreate to clear old logs)
+docker-compose up --force-recreate
