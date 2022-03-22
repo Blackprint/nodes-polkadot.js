eb0155bf046d60b5c02a72c6794c67d01cf540d1
StefansArya <stefansarya1@gmail.com>
2022-03-22 20:40:30 +0700

    Add node to convert Uint8Array to Hex

---
+++ src/Convert/Uint8Array To/Hex.js
@@ -0,0 +1,33 @@
+/**
+ * import { Context } from "../_init.js";
+ * { polkadotUtil } = window
+ */
+
+
+// Register Blackprint Node
+Blackprint.registerNode("Polkadot.js/Convert/Uint8Array To/Hex",
+class RandomSeedNode extends Blackprint.Node {
+	static output = {Out: String};
+	static input = {In: Uint8Array};
+
+	constructor(instance){
+		super(instance);
+
+		let iface = this.setInterface();
+		iface.title = "Uint8Array to Hex";
+	}
+
+	// Event listener can be registered after init
+	init(){
+		// Clear the output port when the input cable was disconnected
+		this.iface.input.In.on('disconnect', Context.EventSlot, ()=> {
+			this.output.Out = null;
+		});
+	}
+
+	// This will be called by the engine if the input port have a new value
+	update(){
+		let { Input, Output } = this.ref; // Shortcut
+		Output.Out = polkadotUtil.u8aToHex(Input.In);
+	}
+});
